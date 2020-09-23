cb599e9beb64ba829897083be3647255b830d236
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-23 15:49:53 -0500

    Built out EventInvite model

--- GatheringAPI/Models/EventInvite.cs
+++ GatheringAPI/Models/EventInvite.cs
@@ -2,5 +2,19 @@
 {
     public class EventInvite
     {
+        public long EventId { get; set; }
+        public long UserId { get; set; }
+
+        public Event Event { get; set; }
+        public User User { get; set; }
+
+        public RSVPStatus Status { get; set; }
+    }
+
+    public enum RSVPStatus
+    {
+        Accept,
+        Decline,
+        Maybe
     }
 }
