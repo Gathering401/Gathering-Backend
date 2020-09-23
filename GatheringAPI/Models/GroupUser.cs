929ce66497a5102fcc4c0b2f39a3c06c571ed3c9
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-23 15:56:58 -0500

    Added groupUser model

---
+++ GatheringAPI/Models/GroupUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GatheringAPI.Models
+{
+    public class GroupUser
+    {
+        public long GroupId { get; set; }
+        public long UserId { get; set; }
+
+        public Group Group { get; set; }
+        public User User { get; set; }
+    }
+}
