450ea85847af387200ed35c3be6eb0934c9525d2
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-23 17:15:24 -0500

    Added migration

---
+++ GatheringAPI/Migrations/20200923221359_AddedUserToGroup.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GatheringAPI.Migrations
+{
+    public partial class AddedUserToGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventInvite_Events_EventId",
+                table: "EventInvite");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventInvite_AspNetUsers_UserId",
+                table: "EventInvite");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupUser_Groups_GroupId",
+                table: "GroupUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupUser_AspNetUsers_UserId",
+                table: "GroupUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GroupUser",
+                table: "GroupUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EventInvite",
+                table: "EventInvite");
+
+            migrationBuilder.RenameTable(
+                name: "GroupUser",
+                newName: "GroupUsers");
+
+            migrationBuilder.RenameTable(
+                name: "EventInvite",
+                newName: "EventInvites");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_GroupUser_UserId",
+                table: "GroupUsers",
+                newName: "IX_GroupUsers_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EventInvite_EventId",
+                table: "EventInvites",
+                newName: "IX_EventInvites_EventId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GroupUsers",
+                table: "GroupUsers",
+                columns: new[] { "GroupId", "UserId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EventInvites",
+                table: "EventInvites",
+                columns: new[] { "UserId", "EventId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventInvites_Events_EventId",
+                table: "EventInvites",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "EventId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventInvites_AspNetUsers_UserId",
+                table: "EventInvites",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupUsers_Groups_GroupId",
+                table: "GroupUsers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupUsers_AspNetUsers_UserId",
+                table: "GroupUsers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventInvites_Events_EventId",
+                table: "EventInvites");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventInvites_AspNetUsers_UserId",
+                table: "EventInvites");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupUsers_Groups_GroupId",
+                table: "GroupUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupUsers_AspNetUsers_UserId",
+                table: "GroupUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GroupUsers",
+                table: "GroupUsers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EventInvites",
+                table: "EventInvites");
+
+            migrationBuilder.RenameTable(
+                name: "GroupUsers",
+                newName: "GroupUser");
+
+            migrationBuilder.RenameTable(
+                name: "EventInvites",
+                newName: "EventInvite");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_GroupUsers_UserId",
+                table: "GroupUser",
+                newName: "IX_GroupUser_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EventInvites_EventId",
+                table: "EventInvite",
+                newName: "IX_EventInvite_EventId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GroupUser",
+                table: "GroupUser",
+                columns: new[] { "GroupId", "UserId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EventInvite",
+                table: "EventInvite",
+                columns: new[] { "UserId", "EventId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventInvite_Events_EventId",
+                table: "EventInvite",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "EventId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventInvite_AspNetUsers_UserId",
+                table: "EventInvite",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupUser_Groups_GroupId",
+                table: "GroupUser",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupUser_AspNetUsers_UserId",
+                table: "GroupUser",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
