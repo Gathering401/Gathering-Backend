aa23d642e79d6b1783781a16ab59e2393bc89772
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-24 17:27:40 -0500

    Added custom exceptions to find empty secrets

---
+++ GatheringAPI/Services/Exceptions/NullTwilioSidException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GatheringAPI.Services
+{
+    [Serializable]
+    internal class NullTwilioSidException : Exception
+    {
+        public NullTwilioSidException() : base("Twilio sid is invalid/empty.")
+        {
+        }
+    }
+}
