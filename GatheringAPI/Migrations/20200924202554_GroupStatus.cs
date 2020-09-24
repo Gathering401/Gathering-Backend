faaa40044512e3a827f007f007c9a82f47fc5378
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-09-24 15:26:40 -0500

    added bool in group.cs

---
+++ GatheringAPI/Migrations/20200924202554_GroupStatus.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GatheringAPI.Migrations
+{
+    public partial class GroupStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "Groups",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "Groups");
+        }
+    }
+}
