c4447db433c55e09e6776974c5424dd537ebf6d4
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-25 09:58:45 -0500

    Added migration for Description of event

---
+++ GatheringAPI/Migrations/20200925144643_AddedEventDescription.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GatheringAPI.Migrations
+{
+    public partial class AddedEventDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Events",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Events");
+        }
+    }
+}
