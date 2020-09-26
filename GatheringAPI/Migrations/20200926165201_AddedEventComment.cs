117b7121444ebec4d2d33ae7ba919ad12746d91a
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-09-26 12:04:41 -0500

    Added migration for comments

---
+++ GatheringAPI/Migrations/20200926165201_AddedEventComment.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GatheringAPI.Migrations
+{
+    public partial class AddedEventComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventComments",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false),
+                    EventId = table.Column<long>(nullable: false),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventComments", x => new { x.EventId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_EventComments_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventComments_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventComments_UserId",
+                table: "EventComments",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventComments");
+        }
+    }
+}
