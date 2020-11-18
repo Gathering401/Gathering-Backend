60630faf38b6ddac0324ceade3b1290f584617b5
Jon Struve <ocdammo3@gmail.com>
2020-11-17 20:42:21 -0600

    Added initial GroupUser controller and repo for modifying groupUser roles

---
+++ GatheringAPI/Controllers/GroupUserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using GatheringAPI.Models;
+using GatheringAPI.Services;
+
+namespace GatheringAPI.Controllers
+{
+    [Route("api/Group")]
+    [ApiController]
+    [Authorize]
+    public class GroupUserController : ControllerBase
+    {
+        
+    }
+}
